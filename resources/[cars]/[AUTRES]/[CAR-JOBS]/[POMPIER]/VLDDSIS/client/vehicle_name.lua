819a984ed4c8eee45f124fbfd4f9fa1e1af6e199
OreoFR <tony37160@hotmail.fr>
2018-08-01 22:19:02 +0200

    Add files via upload

---
+++ resources/[cars]/[AUTRES]/[CAR-JOBS]/[POMPIER]/VLDDSIS/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('VLDDSIS', 'Renault Clio IV Sapeurs Pompiers')
+end)
---
+++ resources/[cars]/[AUTRES]/[CAR-JOBS]/[POMPIER]/x5om/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('FQ2', 'Véhicule de liaison')
+end)
