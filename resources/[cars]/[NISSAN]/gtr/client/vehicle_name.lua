ab7f1d2d137aebcf75d238e8c0d6d87473bccd01
OreoFR <tony37160@hotmail.fr>
2018-08-01 21:51:50 +0200

    Add files via upload

---
+++ resources/[cars]/[NISSAN]/gtr/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('GTR', 'Nissan-GTR-2018')
+end)
---
+++ resources/[cars]/[PEUGEOT]/p205/client/vehicle_name.lua
@@ -0,0 +1,9 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('yPG205t16A', 'Peugeot 205 Turbo'),
+	AddTextEntry('yPG205t16B', 'Peugeot 205 Turbo')
+end)
