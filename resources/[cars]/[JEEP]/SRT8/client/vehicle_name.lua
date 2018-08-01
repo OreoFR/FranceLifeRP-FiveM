527afc9464a618677a41e5cdc3bce1ad5ef04fed
OreoFR <tony37160@hotmail.fr>
2018-08-01 21:42:01 +0200

    Add files via upload

---
+++ resources/[cars]/[JEEP]/SRT8/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('SRT8', 'Jeep SRT-8')
+end)
---
+++ resources/[cars]/[LAMBORGHINI]/lp700r/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('LP700R', 'Lambo Aventador')
+end)
