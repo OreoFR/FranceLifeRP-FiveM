a4d48c270847f021ec33631dbb60e06ec54dd22b
OreoFR <tony37160@hotmail.fr>
2018-08-01 22:23:34 +0200

    Add files via upload

---
+++ resources/[cars]/[AUTRES]/[CAR-JOBS]/[SANTA_MUERTE]/rs6pd600/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('rs6pd600', 'Audi RS6')
+end)
---
+++ resources/[cars]/[AUTRES]/[CAR-JOBS]/stockade2/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('stockade2', 'Camion Blindé')
+end)
