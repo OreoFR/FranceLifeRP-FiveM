df3837d7fdea53230aedad62693f3618e24d3912
OreoFR <tony37160@hotmail.fr>
2018-08-01 21:34:18 +0200

    Add files via upload

---
+++ resources/[cars]/[CITROEN]/cit2cv/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('cit2cv', '2CV')
+end)
---
+++ resources/[cars]/[CITROEN]/citroc4/client/vehicle_name.lua
@@ -0,0 +1,8 @@
+
+
+
+
+Citizen.CreateThread(function()
+	-- Exemple_nom
+	AddTextEntry('citroc4', 'Citroen C4')
+end)
