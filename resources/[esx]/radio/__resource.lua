ab26b9dfa8466d7916ef23d32a37ef7e6ea684de
FuraXx <33160388+FuraXx@users.noreply.github.com>
2018-08-30 21:40:25 +0200

    Update __resource.lua

--- resources/[esx]/radio/__resource.lua
+++ resources/[esx]/radio/__resource.lua
@@ -2,7 +2,7 @@ resource_manifest_version "44febabe-d386-4d18-afbe-5e627f4af937"
 
 -- Example custom radios
 supersede_radio "RADIO_01_CLASS_ROCK" { url = "http://icecast.skyrock.net/s/natio_mp3_64k?tvr_name=frequenceradio&tvr_section1=64mp3", volume = 0.2, name = "Skyrock" }
-supersede_radio "RADIO_02_POP" { url = "http://185.52.127.173/fr/30001/mp3_128.mp3?origine=frequence-radio", volume = 0.2, name = "NRJ" }
+supersede_radio "RADIO_02_POP" { url = "http://185.52.127.155/fr/40001/aac_64.mp3", volume = 0.2, name = "NRJ" }
 supersede_radio "RADIO_03_HIPHOP_NEW" { url = "http://radiofg.impek.com/fg.mp3", volume = 0.2, name = "Radio FG" }
 supersede_radio "RADIO_04_PUNK" { url = "http://streaming.radio.funradio.fr:80/fun-1-44-128.mp3", volume = 0.2, name = "Fun Radio" }
 supersede_radio "RADIO_05_TALK_01" { url = "http://gene-wr05.ice.infomaniak.ch/gene-wr05.mp3", volume = 0.2, name = "Generation" }
